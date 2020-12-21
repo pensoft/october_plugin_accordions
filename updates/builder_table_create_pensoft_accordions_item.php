57f8c5808ad58a3cde833a47814e13fd28ec3a78
root <root@project-dev.pensoft.local>
2020-12-21 12:52:46 +0200

    update migrations

--- updates/builder_table_create_pensoft_accordions_item.php
+++ updates/builder_table_create_pensoft_accordions_item.php
@@ -7,20 +7,26 @@ class BuilderTableCreatePensoftAccordionsItem extends Migration
 {
     public function up()
     {
-        Schema::create('pensoft_accordions_item', function($table)
-        {
-            $table->engine = 'InnoDB';
-            $table->increments('id')->unsigned();
-            $table->string('prefix', 255)->nullable();
-            $table->string('title', 255);
-            $table->text('body')->nullable();
-            $table->timestamp('created_at')->nullable();
-            $table->timestamp('updated_at')->nullable();
-        });
+        if (!Schema::hasTable('pensoft_accordions_item')) {
+            Schema::create('pensoft_accordions_item', function($table)
+            {
+                $table->engine = 'InnoDB';
+                $table->increments('id')->unsigned();
+                $table->string('prefix', 255)->nullable();
+                $table->string('title', 255);
+                $table->text('body')->nullable();
+                $table->timestamp('created_at')->nullable();
+                $table->timestamp('updated_at')->nullable();
+                $table->integer('category_id');
+                $table->integer('sort_order')->nullable();
+            });
+        }
     }
     
     public function down()
     {
-        Schema::dropIfExists('pensoft_accordions_item');
+        if (Schema::hasTable('pensoft_accordions_item')) {
+            Schema::dropIfExists('pensoft_accordions_item');
+        }
     }
 }
