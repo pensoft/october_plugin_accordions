b90df5fccea2c5f6a0adfe98eada49ee4a7159eb
root <root@project-dev.pensoft.local>
2023-03-10 14:30:54 +0200

    revision history

--- models/Category.php
+++ models/Category.php
@@ -1,14 +1,23 @@
 <?php namespace Pensoft\Accordions\Models;
 
 use Model;
-
+use BackendAuth;
 /**
  * Model
  */
 class Category extends Model
 {
     use \October\Rain\Database\Traits\Validation;
-    
+    // For Revisionable namespace
+    use \October\Rain\Database\Traits\Revisionable;
+
+    public $timestamps = false;
+
+    // Add  for revisions limit
+    public $revisionableLimit = 200;
+
+    // Add for revisions on particular field
+    protected $revisionable = ["id","title","content"];
 
     /**
      * @var string The database table used by the model.
@@ -24,4 +33,18 @@ class Category extends Model
     public $hasMany = [
         'items' => Item::class
     ];
+
+    // Add  below relationship with Revision model 
+    public $morphMany = [
+        'revision_history' => ['System\Models\Revision', 'name' => 'revisionable']
+            ];
+    
+    // Add below function use for get current user details
+    public function diff(){
+        $history = $this->revision_history;
+    }
+    public function getRevisionableUser()
+    {
+        return BackendAuth::getUser()->id;
+    }
 }
--- models/Item.php
+++ models/Item.php
@@ -1,7 +1,7 @@
 <?php namespace Pensoft\Accordions\Models;
 
 use Model;
-
+use BackendAuth;
 /**
  * Model
  */
@@ -10,7 +10,16 @@ class Item extends Model
 	use \October\Rain\Database\Traits\Validation;
 	use \October\Rain\Database\Traits\Sortable;
     
+    // For Revisionable namespace
+    use \October\Rain\Database\Traits\Revisionable;
 
+    public $timestamps = false;
+
+    // Add  for revisions limit
+    public $revisionableLimit = 200;
+
+    // Add for revisions on particular field
+    protected $revisionable = ["id","title","content"];
     /**
      * @var string The database table used by the model.
      */
@@ -24,4 +33,18 @@ class Item extends Model
      */
     public $rules = [
     ];
+
+    // Add  below relationship with Revision model 
+    public $morphMany = [
+    'revision_history' => ['System\Models\Revision', 'name' => 'revisionable']
+        ];
+
+    // Add below function use for get current user details
+    public function diff(){
+        $history = $this->revision_history;
+    }
+    public function getRevisionableUser()
+    {
+        return BackendAuth::getUser()->id;
+    }
 }
