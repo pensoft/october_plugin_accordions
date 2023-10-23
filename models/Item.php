dd9e06c73e2079edf49367df0508c53e1842dcd7
root <root@project-dev.pensoft.local>
2023-10-23 12:15:13 +0300

    translate

--- models/Item.php
+++ models/Item.php
@@ -1,6 +1,8 @@
 <?php namespace Pensoft\Accordions\Models;
 
 use Model;
+use BackendAuth;
+use Validator;
 
 /**
  * Model
@@ -9,7 +11,22 @@ class Item extends Model
 {
 	use \October\Rain\Database\Traits\Validation;
 	use \October\Rain\Database\Traits\Sortable;
-    
+
+    // For Revisionable namespace
+    use \October\Rain\Database\Traits\Revisionable;
+
+    public $timestamps = false;
+
+    // Add  for revisions limit
+    public $revisionableLimit = 200;
+
+    // Add for revisions on particular field
+    protected $revisionable = ["id","title","content"];
+
+    /**
+     * @var array Translatable fields
+     */
+    public $translatable = ['title', 'prefix', 'body'];
 
     /**
      * @var string The database table used by the model.
@@ -24,4 +41,53 @@ class Item extends Model
      */
     public $rules = [
     ];
+
+
+    // Add  below relationship with Revision model
+    public $morphMany = [
+    'revision_history' => ['System\Models\Revision', 'name' => 'revisionable']
+        ];
+
+    // Add below function use for get current user details
+    public function diff(){
+        $history = $this->revision_history;
+    }
+    public function getRevisionableUser()
+    {
+        return BackendAuth::getUser()->id;
+    }
+
+    /**
+     * Add translation support to this model, if available.
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        Validator::extend(
+            'json',
+            function ($attribute, $value, $parameters) {
+                json_decode($value);
+
+                return json_last_error() == JSON_ERROR_NONE;
+            }
+        );
+
+        // Call default functionality (required)
+        parent::boot();
+
+        // Check the translate plugin is installed
+        if (!class_exists('RainLab\Translate\Behaviors\TranslatableModel')) {
+            return;
+        }
+
+        // Extend the constructor of the model
+        self::extend(
+            function ($model) {
+
+                // Implement the translatable behavior
+                $model->implement[] = 'RainLab.Translate.Behaviors.TranslatableModel';
+            }
+        );
+    }
 }
